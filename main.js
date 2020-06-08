6cb09f264e1226883c0d96c3575ece7cca3894c8
SaraCremonesi <saracrem00@gmail.com>
2020-06-08 16:49:22 +0200

    inizio esercizio hamburger menu

---
+++ main.js
@@ -0,0 +1,11 @@
+$('.fas.fa-bars').mouseenter(
+  function() {
+  $('.fas.fa-bars').addClass('darkgrey');
+  }
+);
+
+$('.fas.fa-bars').mouseleave(
+  function() {
+  $('.fas.fa-bars').removeClass('darkgrey');
+  }
+);
