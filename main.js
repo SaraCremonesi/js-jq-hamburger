d92fe60706d899e5a9b3f2e8196c53fb4f34eaa9
SaraCremonesi <saracrem00@gmail.com>
2020-06-08 17:22:20 +0200

    completamento esercizio

--- main.js
+++ main.js
@@ -1,11 +1,56 @@
-$('.fas.fa-bars').mouseenter(
-  function() {
-  $('.fas.fa-bars').addClass('darkgrey');
-  }
-);
+// // CAMBIO IL COLORE DELL'ICONA DEL BURGER MENU
+// $('.fas.fa-bars').mouseenter(
+//   function() {
+//     $('.fas.fa-bars').addClass('darkgrey');
+//   }
+// );
+//
+// $('.fas.fa-bars').mouseleave(
+//   function() {
+//   $('.fas.fa-bars').removeClass('darkgrey');
+//   }
+// );
+//
+// // CAMBIO IL COLORE DELL'ICONA DELLA X
+// $('.fas.fa-times').mouseenter(
+//   function() {
+//     $('.fas.fa-times').addClass('darkgrey');
+//   }
+// );
+//
+// $('.fas.fa-times').mouseleave(
+//   function() {
+//   $('.fas.fa-times').removeClass('darkgrey');
+//   }
+// );
 
-$('.fas.fa-bars').mouseleave(
-  function() {
-  $('.fas.fa-bars').removeClass('darkgrey');
-  }
-);
+// // FACCIO APPARIRE E SCOMPARIRE IL MENU AL CLICK DELL'ICONA
+
+// ***************PRIMO MODO***************
+//
+// $('.fas.fa-bars').click(
+//   function() {
+//     $('.hamburger-menu').show()
+//   }
+// );
+//
+// $('.fas.fa-times').click(
+//   function() {
+//     $('.hamburger-menu').hide()
+//   }
+// );
+
+
+// ***************SECONDO MODO***************
+//
+// $('.fas.fa-bars').click(
+//   function() {
+//     $('.hamburger-menu').addClass('active');
+//   }
+// );
+//
+// $('.fas.fa-times').click(
+//   function() {
+//     $('.hamburger-menu').removeClass('active');
+//   }
+// );
